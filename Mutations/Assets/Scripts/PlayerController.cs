1d44619588215a1e916c7b4b752d4b96f713a746
KeatonAllison <alliskea000@gmail.com>
2018-06-02 08:20:10 -0500

    Initial Unity Program

---
+++ Mutations/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb2d.velocity = new Vector2(moveHorizontal * movementSpeed, moveVertical * movementSpeed);
+    }
+}
